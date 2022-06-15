9197f66c55c4be77b87996c000e3fca3d7f4b654
Juan David <juand.ns07@gmail.com>
2022-06-15 16:57:30 -0300

    homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,26 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let decimal = 0;
+  for (let i = 0; i < num.length; i++) {
+    decimal += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binario = []
+  while (num !== 0) {
+    let residuo = parseInt(num % 2);
+    num = Math.floor(num / 2);
+    binario.unshift(residuo)
+  }
+  return binario.join('')
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
